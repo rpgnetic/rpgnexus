ef7b3963465be98dd5990fab7af8edf376b1503a
Wenderson Santos <wenderson.santos@seatecnologia.com.br>
2025-03-02 00:34:57 -0300

    Initial backend commit

---
+++ rpgnexus/src/main/java/br/com/rpgnetic/rpgnexus/RpgnexusApplication.java
@@ -0,0 +1,13 @@
+package br.com.rpgnetic.rpgnexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgnexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgnexusApplication.class, args);
+	}
+
+}
---
+++ rpgnexus/src/test/java/br/com/rpgnetic/rpgnexus/RpgnexusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rpgnetic.rpgnexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgnexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
