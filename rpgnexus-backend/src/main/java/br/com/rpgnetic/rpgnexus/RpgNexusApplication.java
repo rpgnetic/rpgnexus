1e7dc92ce40b7cde69b50598fc13e8d5c34255ed
Wenderson Santos <wenderson.santos@seatecnologia.com.br>
2025-03-04 16:05:01 -0300

    Initial Back-End commit

--- rpgnexus-backend/src/main/java/br/com/rpgnetic/rpgnexus/RpgNexusApplication.java
+++ rpgnexus-backend/src/main/java/br/com/rpgnetic/rpgnexus/RpgNexusApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RpgnexusApplication {
+public class RpgNexusApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RpgnexusApplication.class, args);
+		SpringApplication.run(RpgNexusApplication.class, args);
 	}
 
 }
--- rpgnexus-backend/src/test/java/br/com/rpgnetic/rpgnexus/RpgNexusApplicationTests.java
+++ rpgnexus-backend/src/test/java/br/com/rpgnetic/rpgnexus/RpgNexusApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RpgnexusApplicationTests {
+class RpgNexusApplicationTests {
 
 	@Test
 	void contextLoads() {
